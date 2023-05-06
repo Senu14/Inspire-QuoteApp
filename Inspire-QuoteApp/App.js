339329afb99d798172223078f6d8c6af199ba1f2
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-05-07 01:17:39 +0200

    creating camera on App

--- Inspire-QuoteApp/App.js
+++ Inspire-QuoteApp/App.js
@@ -1,11 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { DailyQuoteCard } from './script';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+
 
 export default function App() {
+  const [hasCameraPermissions, setHasCameraPermission] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back)
+
+  const toggleCameraType = {} => {
+    setType(current => {
+      current === CameraType.back ? CameraType.front : CameraType.back
+    })
+  }
+
+
+  useEffect(()=> {
+       (async () => {
+        MediaLibrary.requestPermissionsAsync()
+        const cameraStatus = await Camera.requestCameraPermissionsAsync()
+        setHasCameraPermission(cameraStatus.status === 'granted')
+       })()
+  })
 
   return (
     <View style={styles.container}>
       <DailyQuoteCard />
+      <Camera style={styles.Camera} type={type}>
+        <View style={styles.camerawrapper}>
+          
+        </View>
+        <View style={styles.controls}>
+          <TouchableOpacity>
+            <text>Tag billede</text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <text>Skift skærm</text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <text>Flash</text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -17,4 +54,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  Camera: {
+    flex: 5,
+    borderRadius: 20
+  },
+  camerawrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30
+
+  },
+  controls: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    paddingBottom: 30,
+    paddingHorizontal: 5,
+    justifyContent: 'space-evenly',
+    height: '100%',
+  }
 });
